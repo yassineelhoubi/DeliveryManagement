c5f0e31c7284b67b5254db083c3c656d1a85237c
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-22 19:22:41 +0100

    refoming managerController structre

--- server/src/api/controllers/managerController.js
+++ server/src/api/controllers/managerController.js
@@ -3,41 +3,57 @@ import Manager from "../models/Manager.js"
 import { createToken } from "../helpers";
 
 
-const createManager = (req, res) => {
+const createManager = async (req, res) => {
 
    const { username, email, password } = req.body
-   const role = "MANAGER"
-   const user = new User({ email, password, role });
-   user.save((err, user) => {
-      if (err) {
-         return res.status(400).json(err)
+
+   try {
+      const userData = {
+         role: "MANAGER",
+         email: email,
+         password: password
       }
+      const user = new User(userData);
+      await user.save()
+      // 
       const managerData = {
          username: username,
          _id: user._id
       }
       const manager = new Manager(managerData);
-      manager.save((err, manager) => {
-         if (err) {
-            return res.status(400).json({ err })
-         }
-         res.json({ user, manager })
+      await manager.save()
+
+      res.status(201).json({
+         status: true,
+         message: { user, manager }
       })
-   })
-   /*  */
-
+   } catch (e) {
+      console.log(e.message)
+      res.status(400).json({
+         status: false,
+         message: e.message
+      })
+   }
 }
+
 const removeManager = async (req, res) => {
+   try {
+      const {
+         id,
+      } = req.params
 
-   const {
-      id,
-   } = req.params
-
-   await User.findOneAndRemove({ _id: id })
-   await Manager.findOneAndRemove({ _id: id })
-   res.status(200).json({
-      msg: "deleted with success"
-   })
+      await User.findOneAndRemove({ _id: id })
+      await Manager.findOneAndRemove({ _id: id })
+      res.status(200).json({
+         status: true,
+         message: "deleted with success"
+      })
+   } catch (e) {
+      res.status(400).json({
+         status: false,
+         message: e.message
+      })
+   }
 }
 
 
