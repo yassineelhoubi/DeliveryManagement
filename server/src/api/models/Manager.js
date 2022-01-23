aaab648029934f3ae538e9055b9a920ae02c1797
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-23 02:39:08 +0100

    add relation between docs: manager and user

--- server/src/api/models/Manager.js
+++ server/src/api/models/Manager.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const managerSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        trim: true,
-        required: true,
-      }
-}, {
-  timestamps: true
-},{collection:"managers"});
-module.exports = mongoose.model('Manager',managerSchema);
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+},
+  {
+    timestamps: true
+  }, { collection: "managers" });
+module.exports = mongoose.model('Manager', managerSchema);
